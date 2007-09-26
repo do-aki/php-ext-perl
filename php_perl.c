3b31d9171d6b91d1d636b9728756a32b6a1c0e2a
dmitry <dmitry@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-09-26 08:29:35 +0000

    Fixed bug #12118 (Compilation error in ZTS mode)
    git-svn-id: https://svn.php.net/repository/pecl/perl/trunk@242933 c90b9560-bf6c-de11-be94-00142212c4b1

--- php_perl.c
+++ php_perl.c
@@ -1779,7 +1779,11 @@ PHP_MINIT_FUNCTION(perl)
   perl_class_entry->get_iterator = php_perl_get_iterator;
 
   INIT_CLASS_ENTRY(perl_ce, "PerlException", NULL);
+#if ZEND_MODULE_API_NO >= 20060613
+  perl_exception_class_entry = zend_register_internal_class_ex(&perl_ce, zend_exception_get_default(TSRMLS_C), NULL TSRMLS_CC);
+#else
   perl_exception_class_entry = zend_register_internal_class_ex(&perl_ce, zend_exception_get_default(), NULL TSRMLS_CC);
+#endif
 
   return SUCCESS;
 }
